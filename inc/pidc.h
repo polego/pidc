35f6b8455e80fd344ffaae381bed88fb269502e0
polego <51285196+polego@users.noreply.github.com>
2022-06-21 15:31:16 +0200

    Add files via upload

---
+++ inc/pidc.h
@@ -0,0 +1,22 @@
+/*
+ * pidc.h
+ *
+ * All rights reserved, Copyright Ogelop AB
+ *
+ * Created on: Jun 21, 2022
+ *     Author: Niclas Wahllöf
+ *
+ */
+
+#ifndef SRC_PIDC_H
+#define SRC_PIDC_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+// Provided services
+
+extern int32_t pidc_adjust(int32_t value);
+
+#endif /* SRC_PIDC_H_ */
+
